4539ac0a6c0acecfbfb94afc2e2d161aa69147d7
ricky2122 <rickyyyy2122@gmail.com>
2024-10-30 15:47:17 +0900

    feat: in French

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 const (
+	french             = "French"
 	spanish            = "Spanish"
 	englishHelloPrefix = "Hello, "
 	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 )
 
 func Hello(name string, language string) string {
@@ -15,6 +17,10 @@ func Hello(name string, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
