6b51a71f104566f88edb7ea6e5ca76c9071514f9
ricky2122 <rickyyyy2122@gmail.com>
2024-11-01 18:40:00 +0900

    feat: Countdown

---
+++ mocking/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer) {
+	for i := 3; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
