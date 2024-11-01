6a074b4ee12f1b24b0b6d7ae93b0a2bf1ff3dc0e
ricky2122 <rickyyyy2122@gmail.com>
2024-11-01 18:58:18 +0900

    feat: Sleeper

--- mocking/main.go
+++ mocking/main.go
@@ -12,14 +12,27 @@ const (
 	countdownStart = 3
 )
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
+	sleeper.Sleep()
 	fmt.Fprint(out, "Go!")
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
