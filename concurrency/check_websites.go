54f8b9f717e31f9ca8776ddd669b5eb73a26f693
ricky2122 <rickyyyy2122@gmail.com>
2024-11-01 21:44:00 +0900

    feat: speeding up CheckWebsites

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,12 +1,28 @@
 package concurrency
 
-type WebsiteChecker func(string) bool
+type (
+	WebsiteChecker func(string) bool
+	result         struct {
+		string
+		bool
+	}
+)
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			// Send statement
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		result := <-resultChannel
+		results[result.string] = result.bool
 	}
 
 	return results
