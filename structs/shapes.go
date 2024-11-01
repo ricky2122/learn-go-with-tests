ac43a2cf04785f05dada93ccca591d9d1a45bf2e
ricky2122 <rickyyyy2122@gmail.com>
2024-11-01 09:59:12 +0900

    refactor: add interface

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,12 +2,16 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func (r *Rectangle) Area() float64 {
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
@@ -15,7 +19,7 @@ type Circle struct {
 	Radius float64
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
