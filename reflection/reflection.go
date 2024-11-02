29842a355d8a17939a398788c4c8ec34e8544b40
ricky2122 <rickyyyy2122@gmail.com>
2024-11-02 10:50:26 +0900

    feat: Support for pointer

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,7 +5,7 @@ import (
 )
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
@@ -13,9 +13,18 @@ func walk(x interface{}, fn func(input string)) {
 		switch field.Kind() {
 		case reflect.String:
 			fn(field.String())
-
 		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
