a2325b9674c0eedd27daa273bafcdd11f0209371
ricky2122 <rickyyyy2122@gmail.com>
2024-11-03 11:12:09 +0900

    feat: Support for V

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -12,6 +12,7 @@ func TestRomanNumerals(t *testing.T) {
 		{Description: "2 gets converted to II", Arabic: 2, Want: "II"},
 		{Description: "3 gets converted to III", Arabic: 3, Want: "III"},
 		{Description: "4 gets converted to IV", Arabic: 4, Want: "IV"},
+		{Description: "5 gets converted to V", Arabic: 5, Want: "V"},
 	}
 
 	for _, test := range cases {
