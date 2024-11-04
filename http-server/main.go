efdcac504cb462e4ecb3d7b44ec26cefa261e834
ricky2122 <rickyyyy2122@gmail.com>
2024-11-04 11:17:51 +0900

    test: Add integration test

--- http-server/main.go
+++ http-server/main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(_ string) int {
-	return 123
+type InMemoryPlayerStore struct {
+	store map[string]int
+	mu    sync.Mutex
 }
 
-func (i *InMemoryPlayerStore) RecordWin(_ string) {}
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{
+		store: map[string]int{},
+	}
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.store[name]++
+}
 
 func main() {
-	server := &PlayerServer{store: &InMemoryPlayerStore{}}
+	server := &PlayerServer{store: NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
